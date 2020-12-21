bf99883b2e553217caee0629fcd53cc71f20bdc1
jakubdonovan <jakubdonovan@protonmail.com>
2020-12-21 09:53:54 +0000

    gameboard design mostly finished

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,16 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+const cssnano = require('cssnano')
+
 module.exports = {
     plugins: [
         require('tailwindcss'),
-        require('autoprefixer')
+        require('autoprefixer'),
+        // cssnano({
+        //     preset: 'default'
+        // }),
+        // purgecss({
+        //     content: ['./public/**/*.html', './public/**/*.js'],
+        //     defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+        // })
     ]
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,53 @@
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    boxShadow: {
+      board: '-20px 20px 20px rgba(0,0,0,.5)',
+      boardHover: '-50px 50px 50px rgba(0,0,0,.5)',
+      layer: '-1px, 1px, 4px rgba(0,0,0,0.1)',
+    },
+    extend: {
+      skew: {
+        '-45': '-45deg'
+      },
+      rotate: {
+        '-60': '-60deg'
+      },
+      colors: {
+        background: '#c8a284',
+        board: {
+          top: '#c7a387',
+          edgeDark: '#81593e',
+          edgeLight: '#a0643e',
+        }
+      },
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      width: ['before', 'after'],
+      height: ['before', 'after'],
+      backgroundColor: ['before', 'after'],
+      position: ['before', 'after'],
+      inset: ['before', 'after'],
+      transform: ['before', 'after'],
+      skew: ['before', 'after'],
+
+    },
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-pseudo-elements'),
+
+    plugin(function ({ addUtilities }) {
+      addUtilities(
+        {
+          '.empty-content': { content: "''"},
+        },
+        ['before', 'after']
+      )
+    }),
+  ],
 }
